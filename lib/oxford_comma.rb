2a68f41e723a9d578841fba92153b7b28cf5c99b
Akshay K <akshaykunti1@gmail.com>
2020-01-03 09:37:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,10 @@ def oxford_comma(array)
   if array.size == 1 
     return array[0]
   elsif array.size == 2 
-    return array.join('and')
+    return array.join(' and ')
   elsif array.size > 2 
     last= array.pop()
-    str = array.join(',')
+    str = array.join(' , ')
     str << ', and '+last
     return str
   else
